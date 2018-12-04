3cae1f265a9a44c6f49666db8341c2c6e2bc1a23
unknown <986834@CS.MUM.EDU>
2018-12-04 13:49:34 -0600

    update member entity class

--- DesignWorkshop/src/member/Member.java
+++ DesignWorkshop/src/member/Member.java
@@ -1,6 +1,49 @@
 package member;
 
 public class Member {
+	public Member(String name, String surname) {
+		this.setName(name);
+		this.setSurname(surname);
+	}
+	
+	public Member(String name, String surname, Address address) {
+		this(name, surname);
+		this.setAddress(address);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	private int id;
 	private String name;
 	private String surname;
 	private Address address;
