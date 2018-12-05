987504b8f67e2dcb4206e7a891f11e43c5f0a8df
umurinan <umurinan@10.10.53.227>
2018-12-05 10:00:56 -0600

    book and author

--- DesignWorkshop/src/application/Main.java
+++ DesignWorkshop/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("../book/AuthorUI.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("../book/BookUI.fxml"));
 			primaryStage.setTitle("Address Form");
 			primaryStage.setScene(new Scene(root, 1200, 1200));
 			// primaryStage.setScene(new Scene(root));
--- DesignWorkshop/src/book/BookInfo.java
+++ DesignWorkshop/src/book/BookInfo.java
@@ -7,8 +7,48 @@ public class BookInfo {
 	private int maxAllowedDays;
 	private String title;
 	private String isbn;
-	
+
 	private ArrayList<BookCopy> copies;
 	private ArrayList<Author> authors;
+
+	public int getMaxAllowedDays() {
+		return maxAllowedDays;
+	}
+
+	public void setMaxAllowedDays(int maxAllowedDays) {
+		this.maxAllowedDays = maxAllowedDays;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public ArrayList<BookCopy> getCopies() {
+		return copies;
+	}
+
+	public void setCopies(ArrayList<BookCopy> copies) {
+		this.copies = copies;
+	}
+
+	public ArrayList<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(ArrayList<Author> authors) {
+		this.authors = authors;
+	}
 
 }
