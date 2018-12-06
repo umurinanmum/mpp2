5f603fa159b22085499adbd550888311c025cbcd
Abdelrady <atantawy@mum.edu>
2018-12-06 10:52:25 -0600

    set main form as startup

--- DesignWorkshop/src/application/Main.java
+++ DesignWorkshop/src/application/Main.java
@@ -13,23 +13,6 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("./Checkout.fxml"));
-			primaryStage.setTitle("Address Form");
-			primaryStage.setScene(new Scene(root, 800, 500));
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		/*try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
 		
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("./MainForm.fxml"));
@@ -41,7 +24,6 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 		
-		*/
 		
 	}
 
